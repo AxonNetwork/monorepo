f5da04644d533d746244ff57462c405635a72243
Bryn Bellomy <bryn.bellomy@gmail.com>
2018-10-04 19:56:18 -0500

    Allow breaking words in threads so UI can adapt to small screens

--- frontend/src/components/Repository/elements/Discussion/Thread.tsx
+++ frontend/src/components/Repository/elements/Discussion/Thread.tsx
@@ -104,6 +104,7 @@ const styles = (theme: Theme) => createStyles({
         display: 'flex',
         flexDirection: 'column',
         height: '100%',
+        wordBreak: 'break-all',
     },
     cancel: {
         position: 'absolute',
