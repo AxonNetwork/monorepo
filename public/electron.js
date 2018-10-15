3b1996f8e578fd55a8070e23e7633f09871d281e
Bryn Bellomy <bryn.bellomy@gmail.com>
2018-10-15 17:38:54 -0500

    Reenable dev tools

--- public/electron.js
+++ public/electron.js
@@ -92,13 +92,13 @@ function startRepoServer() {
     });
 }
 
-// // Add React Dev Tools
-// const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
+// Add React Dev Tools
+const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
 
-// app.on('ready', () => {
-//     [REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS].forEach((extension) => {
-//         installExtension(extension)
-//             .then(name => console.log(`Added Extension: ${name}`))
-//             .catch(err => console.log('An error occurred: ', err));
-//     });
-// });
+app.on('ready', () => {
+    [REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS].forEach((extension) => {
+        installExtension(extension)
+            .then(name => console.log(`Added Extension: ${name}`))
+            .catch(err => console.log('An error occurred: ', err));
+    });
+});
