ef022b966b7f7471383c6be788b6ae0ae3b88a10
Bryn Bellomy <bryn.bellomy@gmail.com>
2019-01-04 19:01:29 -0600

    Make CSV parser more resilient

--- packages/conscience-lib/utils/csv.ts
+++ packages/conscience-lib/utils/csv.ts
@@ -1,5 +1,9 @@
 
-export function parseCSV(contents: string) {
+export function parseCSV(contents: string|null|undefined) {
+    if (!contents) {
+        return []
+    }
+
     const lines = contents.split('\n')
     const data = lines.map(line => line.split(','))
     return data
