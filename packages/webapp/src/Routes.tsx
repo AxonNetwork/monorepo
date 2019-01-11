93a96a2bf0450220194dc721cf5f16664c388674
Daniel Holmgren <dtholmgren@gmail.com>
2019-01-11 02:10:06 -0600

    showcase is a public route + linting

--- packages/webapp/src/Routes.tsx
+++ packages/webapp/src/Routes.tsx
@@ -12,87 +12,87 @@ import ShowcasePage from 'pages/ShowcasePage'
 import { IGlobalState } from 'redux/store'
 
 function Routes(
-	{ loginState, history }: Props
-	){
-	const location = history.location.pathname
-	return (
-		<div>
-			{location !== '/login' &&
-			    <Header />
-			}
-		    <Switch>
-				<Route exact path='/login' component={LoginPage} />
-				<PrivateRoute path='/repo' component={RepoPage} loginState={loginState} />
-				<PrivateRoute path='/settings' component={SettingsPage} loginState={loginState} />
-				<PrivateRoute path='/org' component={OrgPage} loginState={loginState} />
-				<PrivateRoute path='/showcase/:orgID' component={ShowcasePage} loginState={loginState} />
-				<Route render={() => {
-					if(loginState === LoginState.LoggedIn){
-						return <Redirect to='/repo' />
-					}else {
-						return <Redirect to='/login' />
-					}
-				}}
-				/>
-		    </Switch>
-		    <Footer />
-		</div>
-	)
+    { loginState, history }: Props,
+    ) {
+    const location = history.location.pathname
+    return (
+        <div>
+            {location !== '/login' &&
+                <Header />
+            }
+            <Switch>
+                <Route exact path="/login" component={LoginPage} />
+                <PrivateRoute path="/repo" component={RepoPage} loginState={loginState} />
+                <PrivateRoute path="/settings" component={SettingsPage} loginState={loginState} />
+                <PrivateRoute path="/org" component={OrgPage} loginState={loginState} />
+                <Route path="/showcase/:orgID" component={ShowcasePage} />
+                <Route render={() => {
+                    if (loginState === LoginState.LoggedIn) {
+                        return <Redirect to="/repo" />
+                    }else {
+                        return <Redirect to="/login" />
+                    }
+                }}
+                />
+            </Switch>
+            <Footer />
+        </div>
+    )
 }
 
 enum LoginState {
-	Checking,
-	LoggedIn,
-	NotLoggedIn
+    Checking,
+    LoggedIn,
+    NotLoggedIn,
 }
 
 interface Props {
-	loginState: LoginState
-	history: History
+    loginState: LoginState
+    history: History
 }
 
 export function PrivateRoute({ component: Component, loginState, ...rest}: any) {
-	return (
-		<Route {...rest} render={(props) => {
-			if(loginState === LoginState.LoggedIn) {
-				return <Component {...props} />
-			}else if(loginState === LoginState.NotLoggedIn){
-				return(
-					<Redirect
-						to={{
-			              pathname: "/login",
-			              state: { from: props.location }
-			            }}
-					 />
-				)
-			}else {
-				return null
-			}
-		}} />
-	)
+    return (
+        <Route {...rest} render={(props) => {
+            if (loginState === LoginState.LoggedIn) {
+                return <Component {...props} />
+            }else if (loginState === LoginState.NotLoggedIn) {
+                return(
+                    <Redirect
+                        to={{
+                          pathname: '/login',
+                          state: { from: props.location },
+                        }}
+                     />
+                )
+            }else {
+                return null
+            }
+        }} />
+    )
 }
 
 const mapStateToProps = (state: IGlobalState) => {
-	let loginState = LoginState.NotLoggedIn
-	if(!state.user.checkedLoggedIn){
-		loginState = LoginState.Checking
-	}
-	if(state.user.currentUser !== undefined) {
-		loginState = LoginState.LoggedIn
-	}
+    let loginState = LoginState.NotLoggedIn
+    if (!state.user.checkedLoggedIn) {
+        loginState = LoginState.Checking
+    }
+    if (state.user.currentUser !== undefined) {
+        loginState = LoginState.LoggedIn
+    }
 
     return {
-    	loginState
+        loginState,
     }
 }
 
 const mapDispatchToProps = {}
 
 const RoutesContainer = withRouter(
-	connect(
-	    mapStateToProps,
-	    mapDispatchToProps,
-	)(Routes) as any
+    connect(
+        mapStateToProps,
+        mapDispatchToProps,
+    )(Routes) as any,
 )
 
 export default RoutesContainer
