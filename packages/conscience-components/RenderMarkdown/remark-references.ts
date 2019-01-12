1cfc35bdedd071125f9c34d036917b0edf65f1b8
Bryn Bellomy <bryn.bellomy@gmail.com>
2019-01-12 16:23:12 -0600

    Fix @shortcode embeds with forward slashes

--- packages/conscience-components/RenderMarkdown/remark-references.ts
+++ packages/conscience-components/RenderMarkdown/remark-references.ts
@@ -5,7 +5,7 @@
  * Helpful guide: https://github.com/remarkjs/remark/tree/master/packages/remark-parse#parserinlinetokenizers
  */
 
-const REGEX = /@(\w+):\[([a-zA-Z0-9="',\-:\. ]+)\]/
+const REGEX = /@(\w+):\[([a-zA-Z0-9="',\-:\. \/]+)\]/
 
 export default function shortcodes(this: any) {
     if (isRemarkParser(this.Parser)) {
